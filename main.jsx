d8fcdbcb9272f31789a0736889612ee0a115a3f2
testposeyy-dev <test.poseyy@gmail.com>
2026-03-12 21:32:46 +0100

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Portofolio from './Portofolio.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Portofolio />
+  </React.StrictMode>,
+)
